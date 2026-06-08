826df95eae639f690861a4531cc32ea8a6679721
LorenzoDolfi <ldolfi3@gatech.edu>
2026-06-08 14:26:42 -0400

    Test smaller garden splat

--- src/main.js
+++ src/main.js
@@ -507,7 +507,7 @@ export class MuJoCoDemo {
 
       this.scene.add(this.splatViewer);
 
-      await this.splatViewer.addSplatScene("/assets/splats/garden.ksplat", {
+      await this.splatViewer.addSplatScene("/assets/splats/test.ksplat", {
         splatAlphaRemovalThreshold: 1,
         showLoadingUI: false,
         position: [1, 1.05, 0],
